910b4b270a665a0516157d28e7aa98fdea82ed7e
Yorch Sanchez <dybsm98@outlook.es>
2024-06-28 17:50:26 -0500

    se elimina el botón

--- src/components/users/HeaderTable.jsx
+++ src/components/users/HeaderTable.jsx
@@ -1,9 +1,4 @@
-export default function HeaderTable({
-  title,
-  description,
-  buttonText,
-  onButtonClick
-}) {
+export default function HeaderTable({ title, description }) {
   return (
     <div className='sm:flex sm:items-center'>
       <div className='sm:flex-auto'>
@@ -11,14 +6,6 @@ export default function HeaderTable({
           {title}
         </h1>
         <p className='mt-2 text-sm text-gray-700'>{description}</p>
-      </div>
-      <div className='mt-4 sm:ml-16 sm:mt-0 sm:flex-none'>
-        <button
-          type='button'
-          onClick={onButtonClick}
-          className='block rounded-md bg-indigo-600 px-3 py-2 text-center text-sm font-semibold text-white shadow-sm hover:bg-indigo-500 focus-visible:outline focus-visible:outline-2 focus-visible:outline-offset-2 focus-visible:outline-indigo-600'>
-          {buttonText}
-        </button>
       </div>
     </div>
   )
