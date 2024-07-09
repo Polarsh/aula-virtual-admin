2cca1a1615c06ea7d504e7284c4c226fa7f89ed0
Yorch Sanchez <dybsm98@outlook.es>
2024-07-09 13:52:37 -0500

    modificacion para columna de acciones

--- src/components/shared/Table/TableBody.jsx
+++ src/components/shared/Table/TableBody.jsx
@@ -4,7 +4,6 @@ import {
   ChevronUpDownIcon,
   ChevronUpIcon
 } from '@heroicons/react/20/solid'
-import TableAction from './TableAction'
 
 export default function TableBody({ table, actionItems }) {
   const rowModel = table.getRowModel()
@@ -81,11 +80,26 @@ export default function TableBody({ table, actionItems }) {
                           )}
                         </td>
                       ))}
-                      <td className='relative whitespace-nowrap py-4 pl-3 pr-4 text-right text-sm font-medium sm:pr-6'>
-                        <TableAction
-                          actionItems={actionItems}
-                          id={row.original.id}
-                        />
+                      <td className='relative whitespace-nowrap py-2 text-right text-sm font-medium sm:pr-6'>
+                        <div className='flex space-x-2'>
+                          {actionItems.map((item, index) => (
+                            <button
+                              key={index}
+                              onClick={() => {
+                                item.action(row.original.id)
+                              }}
+                              className={`group flex items-center p-2 text-sm text-gray-700 data-[focus]:bg-gray-100 data-[focus]:text-gray-900 ${index > 0 ? 'hidden sm:flex' : 'flex'}`}>
+                              <item.icon
+                                aria-hidden='true'
+                                className='h-5 w-5 text-gray-400 group-hover:text-gray-500'
+                              />
+                              {/* Texto visible solo en pantallas medianas y grandes */}
+                              <span className='ml-2 hidden sm:inline truncate'>
+                                {item.label}
+                              </span>
+                            </button>
+                          ))}
+                        </div>
                       </td>
                     </tr>
                   ))
