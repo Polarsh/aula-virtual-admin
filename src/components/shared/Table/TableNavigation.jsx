67e457fc78df2bba8c398e68458e134dc00dcc00
Yorch Sanchez <dybsm98@outlook.es>
2024-07-09 17:13:36 -0500

    Se puede modificacion total de rows

--- src/components/shared/Table/TableNavigation.jsx
+++ src/components/shared/Table/TableNavigation.jsx
@@ -4,8 +4,21 @@ export default function TableNavigation({ table }) {
   return (
     <nav
       aria-label='Pagination'
-      className='flex items-center justify-between border-t border-gray-200 px-4 py-3 sm:px-6'>
-      <div className='hidden sm:block'>
+      className='flex items-center justify-between border-t border-gray-200'>
+      <select
+        className='block rounded-md border-0 py-1.5 text-gray-900 ring-1 ring-inset ring-gray-300 focus:ring-2 focus:ring-my-primary sm:text-sm sm:leading-6'
+        value={table.getState().pagination.pageSize}
+        onChange={e => {
+          table.setPageSize(Number(e.target.value))
+        }}>
+        {[10, 20, 30].map(pageSize => (
+          <option key={pageSize} value={pageSize}>
+            Mostrar {pageSize}
+          </option>
+        ))}
+      </select>
+
+      <div className='hidden sm:block ml-3'>
         <p className='text-sm text-gray-700'>
           Mostrando{' '}
           <span className='font-medium'>
