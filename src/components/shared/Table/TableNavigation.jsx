3c9c19eaab0cbe161c8d3b2f758eb562be97970e
Yorch Sanchez <dybsm98@outlook.es>
2024-07-09 12:16:02 -0500

    cambio de diseño

--- src/components/shared/Table/TableNavigation.jsx
+++ src/components/shared/Table/TableNavigation.jsx
@@ -1,56 +1,50 @@
-import { ChevronLeftIcon, ChevronRightIcon } from '@heroicons/react/20/solid'
+import CustomButton, { ButtonStyle } from '../Buttons/Buttons'
 
 export default function TableNavigation({ table }) {
   return (
-    <div className='flex items-center justify-between border-gray-200 px-4 sm:px-6'>
-      <div className='flex flex-1 items-center justify-between'>
-        <div>
-          <p className='text-sm text-gray-700'>
-            Mostrando{' '}
-            <span className='font-medium'>
-              {Math.min(
-                table.getState().pagination.pageIndex *
-                  table.getState().pagination.pageSize +
-                  1,
-                table.getFilteredRowModel().rows.length
-              )}
-            </span>{' '}
-            a{' '}
-            <span className='font-medium'>
-              {Math.min(
-                (table.getState().pagination.pageIndex + 1) *
-                  table.getState().pagination.pageSize,
-                table.getFilteredRowModel().rows.length
-              )}
-            </span>{' '}
-            de{' '}
-            <span className='font-medium'>
-              {table.getFilteredRowModel().rows.length}
-            </span>{' '}
-            resultados
-          </p>
-        </div>
-        <div>
-          <nav
-            className='isolate inline-flex -space-x-px rounded-md shadow-sm'
-            aria-label='Pagination'>
-            <button
-              onClick={() => table.previousPage()}
-              disabled={!table.getCanPreviousPage()}
-              className='relative inline-flex items-center rounded-l-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 disabled:opacity-50 disabled:cursor-not-allowed'>
-              <span className='sr-only'>Anterior</span>
-              <ChevronLeftIcon className='h-5 w-5' aria-hidden='true' />
-            </button>
-            <button
-              onClick={() => table.nextPage()}
-              disabled={!table.getCanNextPage()}
-              className='relative inline-flex items-center rounded-r-md px-2 py-2 text-gray-400 ring-1 ring-inset ring-gray-300 hover:bg-gray-50 focus:z-20 focus:outline-offset-0 disabled:opacity-50 disabled:cursor-not-allowed'>
-              <span className='sr-only'>Siguiente</span>
-              <ChevronRightIcon className='h-5 w-5' aria-hidden='true' />
-            </button>
-          </nav>
-        </div>
+    <nav
+      aria-label='Pagination'
+      className='flex items-center justify-between border-t border-gray-200 bg-white px-4 py-3 sm:px-6'>
+      <div className='hidden sm:block'>
+        <p className='text-sm text-gray-700'>
+          Mostrando{' '}
+          <span className='font-medium'>
+            {Math.min(
+              table.getState().pagination.pageIndex *
+                table.getState().pagination.pageSize +
+                1,
+              table.getFilteredRowModel().rows.length
+            )}
+          </span>{' '}
+          a{' '}
+          <span className='font-medium'>
+            {Math.min(
+              (table.getState().pagination.pageIndex + 1) *
+                table.getState().pagination.pageSize,
+              table.getFilteredRowModel().rows.length
+            )}
+          </span>{' '}
+          de{' '}
+          <span className='font-medium'>
+            {table.getFilteredRowModel().rows.length}
+          </span>{' '}
+          resultados
+        </p>
       </div>
-    </div>
+      <div className='flex flex-1 justify-between sm:justify-end gap-3'>
+        <CustomButton
+          onClick={() => table.previousPage()}
+          label={'Anterior'}
+          disabled={!table.getCanPreviousPage()}
+          variant={ButtonStyle.Text}
+        />
+        <CustomButton
+          onClick={() => table.nextPage()}
+          label={'Siguiente'}
+          disabled={!table.getCanNextPage()}
+          variant={ButtonStyle.Text}
+        />
+      </div>
+    </nav>
   )
 }
